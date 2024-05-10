02a1ef4b693c1c3f53401b24309d4cc7a0313334
Ng Man Ho <142741897+AlvinNgMH@users.noreply.github.com>
2024-05-11 02:10:08 +0800

    Update Connect-via-PolyU-Library.user.js

--- Connect-via-PolyU-Library.user.js
+++ Connect-via-PolyU-Library.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Connect via PolyU Library
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Add Connect via PolyU Library to context menu
 // @icon         https://www.lib.polyu.edu.hk/sites/all/themes/polyu85/favicon.ico
-// @home         https://github.com/AlvinNgMH/UserScript
 // @downloadURL  https://raw.githubusercontent.com/AlvinNgMH/UserScript/main/Connect-via-PolyU-Library.user.js
 // @updateURL    https://raw.githubusercontent.com/AlvinNgMH/UserScript/main/Connect-via-PolyU-Library.user.js
 // @author       Alvin
